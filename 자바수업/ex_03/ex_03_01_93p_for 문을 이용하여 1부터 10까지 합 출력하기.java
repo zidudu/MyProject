53d653486b00a04263077cd2e6411df1c693d57a
ijigu <75564572+zidudu@users.noreply.github.com>
2025-03-25 13:22:34 +0900

    Update ex_03_01_93p_for 문을 이용하여 1부터 10까지 합 출력하기.java

--- 자바수업/ex_03/ex_03_01_93p_for 문을 이용하여 1부터 10까지 합 출력하기.java
+++ 자바수업/ex_03/ex_03_01_93p_for 문을 이용하여 1부터 10까지 합 출력하기.java
@@ -1,1 +1,30 @@
-public
+import java.util.Scanner;
+public class Hello2030 {	
+	
+	//메인 함수
+	public static void main(String[] args) {
+		int i,sum=0;
+		for(i=1;i<=10;i++) { //1~10까지 반복
+			sum+=i;
+			System.out.print(i); // 더하는 수 출력
+			if(i<=9) //1~0까지는 + 출력
+				System.out.print("+");
+			else { // i 10인경우
+				System.out.print("=");
+				System.out.print(sum);
+			}
+		}
+		
+		
+	
+		
+	}
+	}
+
+
+/*
+ 
+
+1+2+3+4+5+6+7+8+9+10==55
+
+*/
